2f65b239ba79067f71d56134b0ce661439031352
Christian Thäter <ct@pipapo.org>
2021-02-15 05:43:00 +0100

    Documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,27 @@
+//! Low level interface to C-API's that expect a pointer/size reference to a buffer
+//!
+//! In many libc API's it is common to pass a pointer/size pair into a function.
+//! This describes the location and length of data to be read or written.
+//!
+//! This library provides tools to generate such pointer/size pairs in rust.  In rust these
+//! are either vectors or array of 'u8' values.  Buffers used for reading data in can be
+//! defined as 'uninit_array!' to reduce the overhead of unnnecessary initialization.
+//!
 #![feature(maybe_uninit_slice)]
-
 use std::vec::Vec;
 use libc::{c_char, c_void, size_t};
 use std::mem::MaybeUninit;
 
-
+/// Basic functionality to get the used and allocated size of a buffer
 pub trait CBuffer {
+    /// The used length
     fn len(&self) -> usize;
+
+    /// The allocated size
     fn capacity(&self) -> usize;
 }
 
-
+/// Shared functionality to query length and capacity of the underlying buffer.
 impl CBuffer for Vec<u8> {
     fn len(&self) -> usize {
         self.len()
@@ -47,11 +58,13 @@ impl<const N: usize> CBuffer for [MaybeUninit<u8>; N] {
 
 
 
-
-// an initialized buffer: array, slice or vec
+/// The 'TxBuffer' is a used when writing data out.
+/// It's contents must be fully initialized.
 pub trait TxBuffer: CBuffer {
+    /// Returns a const void*/size_t pair to be used in the C call.
     fn as_c_void (&self) -> (*const c_void, size_t);
 
+    /// Returns a const char*/size_t pair to be used in the C call.
     fn as_c_char (&self) -> (*const c_char, size_t);
 }
 
@@ -81,12 +94,19 @@ impl<const N: usize> TxBuffer for [u8; N] {
 
 
 
-// an possibly uninitialized: buffer array or vec
+/// The 'RxBuffer' is a used when reading data in.
+/// The contents of the buffer can be uninitialized.
 pub trait RxBuffer: CBuffer {
+    /// Returns a void*/size_t pair to be used in the C call.
     fn as_c_void (&mut self) -> (*mut c_void, size_t);
 
+    /// Returns a char*/size_t pair to be used in the C call.
     fn as_c_char (&mut self) -> (*mut c_char, size_t);
 
+    /// After the reading operation is done the buffer must be sealed
+    /// with the actual length of the data retrieved.
+    /// This function returns s slice into the buffer containing the
+    /// data.
     fn rx_done(&mut self, len: size_t) -> &[u8];
 }
 
