a8393c9a932236febae3595f0ef661a65a99fd66
Christian Thäter <ct@pipapo.org>
2021-02-15 05:41:59 +0100

    cosmetics: use libc::size_t on the C side

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![feature(maybe_uninit_slice)]
 
 use std::vec::Vec;
-use libc::{c_char, c_void};
+use libc::{c_char, c_void, size_t};
 use std::mem::MaybeUninit;
 
 
@@ -50,27 +50,28 @@ impl<const N: usize> CBuffer for [MaybeUninit<u8>; N] {
 
 // an initialized buffer: array, slice or vec
 pub trait TxBuffer: CBuffer {
-    fn as_c_void (&self) -> (*const c_void, usize);
-    fn as_c_char (&self) -> (*const c_char, usize);
+    fn as_c_void (&self) -> (*const c_void, size_t);
+
+    fn as_c_char (&self) -> (*const c_char, size_t);
 }
 
 
 impl TxBuffer for Vec<u8> {
-   fn as_c_void (&self) -> (*const c_void, usize){
+   fn as_c_void (&self) -> (*const c_void, size_t){
         (self.as_ptr() as *mut c_void, self.len())
     }
 
-    fn as_c_char (&self) -> (*const c_char, usize){
+    fn as_c_char (&self) -> (*const c_char, size_t){
         (self.as_ptr() as *mut c_char, self.len())
     }
 }
 
 impl<const N: usize> TxBuffer for [u8; N] {
-    fn as_c_void (&self) -> (*const c_void, usize){
+    fn as_c_void (&self) -> (*const c_void, size_t){
         (self.as_ptr() as *mut c_void, self.len())
     }
 
-    fn as_c_char (&self) -> (*const c_char, usize){
+    fn as_c_char (&self) -> (*const c_char, size_t){
         (self.as_ptr() as *mut c_char, self.len())
     }
 }
@@ -82,20 +83,22 @@ impl<const N: usize> TxBuffer for [u8; N] {
 
 // an possibly uninitialized: buffer array or vec
 pub trait RxBuffer: CBuffer {
-    fn as_c_void (&mut self) -> (*mut c_void, usize);
-    fn as_c_char (&mut self) -> (*mut c_char, usize);
-    fn rx_done(&mut self, len: usize) -> &[u8];
+    fn as_c_void (&mut self) -> (*mut c_void, size_t);
+
+    fn as_c_char (&mut self) -> (*mut c_char, size_t);
+
+    fn rx_done(&mut self, len: size_t) -> &[u8];
 }
 
 
 
 impl RxBuffer for Vec<u8> {
-    fn as_c_void (&mut self) -> (*mut c_void, usize){
+    fn as_c_void (&mut self) -> (*mut c_void, size_t){
         unsafe {self.set_len(self.capacity())};
         (self.as_mut_ptr() as *mut c_void, self.len())
     }
 
-    fn as_c_char (&mut self) -> (*mut c_char, usize){
+    fn as_c_char (&mut self) -> (*mut c_char, size_t){
         (self.as_mut_ptr() as *mut c_char, self.len())
     }
 
@@ -109,11 +112,11 @@ impl RxBuffer for Vec<u8> {
 
 
 impl<const N: usize> RxBuffer for [u8; N] {
-    fn as_c_void (&mut self) -> (*mut c_void, usize){
+    fn as_c_void (&mut self) -> (*mut c_void, size_t){
         (self.as_mut_ptr() as *mut c_void, N)
     }
 
-    fn as_c_char (&mut self) -> (*mut c_char, usize){
+    fn as_c_char (&mut self) -> (*mut c_char, size_t){
         (self.as_mut_ptr() as *mut c_char, N)
     }
 
@@ -123,11 +126,11 @@ impl<const N: usize> RxBuffer for [u8; N] {
 }
 
 impl<const N: usize> RxBuffer for [MaybeUninit<u8>; N] {
-    fn as_c_void (&mut self) -> (*mut c_void, usize){
+    fn as_c_void (&mut self) -> (*mut c_void, size_t){
         (self.as_mut_ptr() as *mut c_void, N)
     }
 
-    fn as_c_char (&mut self) -> (*mut c_char, usize){
+    fn as_c_char (&mut self) -> (*mut c_char, size_t){
         (self.as_mut_ptr() as *mut c_char, N)
     }
 
